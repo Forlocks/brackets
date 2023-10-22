987152f4abffa53a676894968b3e67c1d8644bf9
Forlocks <volgaopa@gmail.com>
2023-10-22 14:36:23 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(str);
+  console.log(bracketsConfig);
+
+  if (str.length < 2) {
+    return false;
+  } else {
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (str[i] == bracketsConfig[j][1] && stack.length !== 0 && stack[stack.length - 1] === bracketsConfig[j][0]) { // Если закрывающая скобка, стек не пустой и top - нужная пара
+          stack.pop();
+        } else if (str[i] == bracketsConfig[j][0]) { // Если открывающая скобка
+          stack.push(str[i]);
+        } else if (str[i] == bracketsConfig[j][1] && stack.length === 0) { // Если закрывающая скобка и стек пустой
+          return false;
+        }
+      }
+    }
+
+    console.log(stack);
+
+    if (stack.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
